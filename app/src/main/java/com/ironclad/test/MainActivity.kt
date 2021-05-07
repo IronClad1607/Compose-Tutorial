6b61db4054629a5423e629badf56dd45837238a5
Ishaan Jain <ishaan160799@gmail.com>
2021-05-07 20:24:42 +0530

    Project started

---
+++ app/src/main/java/com/ironclad/test/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ironclad.test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text("Hello World!")
+        }
+    }
+}
