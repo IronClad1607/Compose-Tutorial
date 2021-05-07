2fc55fa3fcc9d343cbc41bf877184c7bf24f6df3
Ishaan Jain <ishaan160799@gmail.com>
2021-05-07 20:29:27 +0530

    preview function

--- app/src/main/java/com/ironclad/test/MainActivity.kt
+++ app/src/main/java/com/ironclad/test/MainActivity.kt
@@ -4,12 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hello World!")
+            Greeting("Android")
         }
+    }
+
+    @Composable
+    fun Greeting(name: String){
+        Text(text = "Hello $name")
+    }
+    
+    @Preview
+    @Composable
+    fun PreviewGreeting(){
+        Greeting(name = "Android")
     }
 }
